b29573b7b8e41ea28fc1da528862cc8151d2a084
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-24 22:49:51 +0200

    Update script

--- script.js
+++ script.js
@@ -3,65 +3,110 @@ var movies = [
 		id: 1,
 		title: 'Harry Potter',
 		desc: 'film o czarodzieju',
-		posterImg: 'https://ae01.alicdn.com/kf/HTB1XU9TOVXXXXbiapXXq6xXFXXXn/Obraz-na-p-tnie-na-zam-wienie-Harry-Potter-plakat-Harry-Potter-naklejki-cienne-Harry-Porter.jpg'
+		src: 'https://ae01.alicdn.com/kf/HTB1XU9TOVXXXXbiapXXq6xXFXXXn/Obraz-na-p-tnie-na-zam-wienie-Harry-Potter-plakat-Harry-Potter-naklejki-cienne-Harry-Porter.jpg'
 	},
 	{
 		id: 2,
 		title: 'Król Lew',
 		desc: 'Film o królu sawanny',
-		posterImg: 'https://ssl-gfx.filmweb.pl/po/63/16/776316/7892306.3.jpg'
+		src: 'https://ssl-gfx.filmweb.pl/po/63/16/776316/7892306.3.jpg'
 	},
 	{
 		id: 3,
 		title: 'Joker',
 		desc: 'Amerykański dramat psychologiczny',
-		posterImg: 'http://t0.gstatic.com/images?q=tbn:ANd9GcSKQyxhp32zymjvpPFyoyplR35o20qpkh0raONZ_JsTG7Gf1w78'
+		src: 'http://t0.gstatic.com/images?q=tbn:ANd9GcSKQyxhp32zymjvpPFyoyplR35o20qpkh0raONZ_JsTG7Gf1w78'
 	},
 	{
 		id: 4,
 		title: 'Szybcy i wściekli 8',
 		desc: 'Amerykański film akcji',
-		posterImg: 'https://csn.naekranie.pl/wp-content/uploads/2015/04/fast.jpg' 
+		src: 'https://csn.naekranie.pl/wp-content/uploads/2015/04/fast.jpg' 
 
 	}
 ];
 
 var Movie = React.createClass({
-	propTypes: {
-		movie: React.PropTypes.object.isRequired,
-	},
-	render: function(){
-		return (
-			React.createElement('li', {},
-				React.createElement('h2', {}, this.props.movie.title),
-				React.createElement('p', {}, this.props.movie.desc),
-				React.createElement('img', { src: this.props.movie.posterImg, alt: 'plakat filmu' })
-			)
-		);
-	}
 
+    propTypes: {
+        movie: React.PropTypes.object.isRequired,
+    },
+
+    render() {
+
+        var movie = this.props.movie;
+
+        return React.createElement('li', {},
+                React.createElement(MovieTitle, {title: movie.title}),
+                React.createElement(MovieDesc, {desc: movie.desc}),
+                React.createElement(MovieImage, {src: movie.src, alt: movie.title})
+        );
+    }
 });
 
-var MovieList = React.createClass({
-	propTypes: {
-		movies: React.PropTypes.array.isRequired,
-	},
-	render: function(){
-		var movies = this.props.movies.map(function(movie){
-			return React.createElement(Movie, {movie, key: movie.id})
-		});
-
-		return (
-			React.createElement('div', {},
-				React.createElement('h1', {}, 'Lista filmów'),
-				React.createElement('ul', {}, movies)
-			)
-		);
-	}
+var MovieTitle = React.createClass({
+    propTypes: {
+        title: React.PropTypes.string,
+    },
+    render() {
+        return (
+            React.createElement('h2', {}, this.props.title)
+        )
+    },
 });
 
-var element = React.createElement(MovieList, {movies: movies});
+var MovieDesc = React.createClass({
+    propTypes: {
+        desc: React.PropTypes.string,
+    },
+
+    render() {
+        return (
+           React.createElement('p', {}, this.props.desc)
+        )
+    },
+})
+
+var MovieImage = React.createClass({
+    propTypes: {
+        src: React.PropTypes.string,
+        alt: React.PropTypes.string
+    },
+    render() {
+        return (
+            React.createElement('img', {src: this.props.src, alt: this.props.alt})
+        )
+    },
+});
+
+var MoviesList = React.createClass({
+    render() {
+        var moviesElements = movies.map(function (movie) {
+            return React.createElement(Movie, {
+                key: movie.id,
+                movie: movie
+            });
+        })
+
+        return React.createElement('div', {},
+                React.createElement('h1', {}, 'Lista filmów'),
+                React.createElement('ul', {}, moviesElements)
+        )
+    }
+});
+
+var element = React.createElement(MoviesList);
 ReactDOM.render(element, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
+
 
 
 
